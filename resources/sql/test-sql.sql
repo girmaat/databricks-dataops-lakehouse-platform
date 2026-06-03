0f2a6538579c58039fff626d64e9323c1dbcce6a
girmaat <girmaat@gmail.com>
2026-06-03 21:59:20 +0000

    Add assignment event Bronze DLT ingestion pipeline

--- resources/sql/test-sql.sql
+++
@@ -1,7 +0,0 @@
-USE CATALOG adb_classic_compute_catalog;
-
-CREATE SCHEMA IF NOT EXISTS monitoring;
-
-CREATE VOLUME IF NOT EXISTS monitoring.checkpoints;
-
-SHOW VOLUMES IN adb_classic_compute_catalog.monitoring;
